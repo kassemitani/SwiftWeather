273859bafc8a54c652ac8e44fb19f6939d32d05f
kassem itani <kassem.itani@gmail.com>
2017-12-28 13:51:36 +0300

    Initial Commit

---
+++ SwiftWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWeather
+//
+//  Created by Kassem Itani on 12/28/17.
+//  Copyright © 2017 kassem Itani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
